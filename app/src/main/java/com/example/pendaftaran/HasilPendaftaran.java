5bf2e2733ffd7d14d731d9a8ce2fde99925f382a
faryna03 <nabilazhafarina.nz@gmail.com>
2022-09-24 18:40:55 +0700

    praktek ujikom

---
+++ app/src/main/java/com/example/pendaftaran/HasilPendaftaran.java
@@ -0,0 +1,14 @@
+package com.example.pendaftaran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HasilPendaftaran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil_pendaftaran);
+    }
+}
